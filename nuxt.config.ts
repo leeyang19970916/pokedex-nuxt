9a5c07ef8b62d62ed76b1423a505214d5618a9a6
leeyang19970916 <91140699+leeyang19970916@users.noreply.github.com>
2026-03-09 21:37:36 +0800

    connection github

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// nuxt.config.ts
+export default defineNuxtConfig({
+  // 讓 Nuxt 知道你要用這些超能力
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@element-plus/nuxt",
+    "@nuxt/eslint",
+  ],
+
+  // SCSS 全域變數配置 (如果你有全域變數檔)
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/styles/main.scss" as *;',
+        },
+      },
+    },
+  },
+
+  // Element Plus 設定 (可選)
+  elementPlus: {
+    icon: "ElIcon", // 自動引入 Icon
+    importStyle: "scss", // 讓 Element Plus 配合 SCSS
+  },
+});
