360a5d991f34f0c6509eb0bea8291429433a3267
leeyang19970916 <91140699+leeyang19970916@users.noreply.github.com>
2026-03-11 01:13:28 +0800

    chore: pikmin bg-image

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,7 @@
 <template>
   <div>12313</div>
+  <img :src="DDD" alt="" />
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DDD from "@/assets/image/image.png";
+</script>
