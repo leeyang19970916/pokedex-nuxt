aa303ee29404350ac3e3aafcad358fdb74dd063a
YangLee <yan.lee@guest.kkbox.group>
2026-03-29 15:46:27 +0800

    fix: delete FakeImage

--- app/layouts/custom.vue
+++ app/layouts/custom.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <img :src="FakeBg" alt="" />
     <slot></slot>
   </div>
 </template>
-<script setup lang="ts">
-import FakeBg from "~/assets/image/fake_bg.png";
-</script>
+<script setup lang="ts"></script>
